884271484a19e3ff105e3a5e604bc09473670771
Nhat Duong <duongnguyenkt99@gmail.com>
2025-06-02 21:02:49 +0700

    Learned and installed Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-
-import './App.css'
-
+import Button from '@mui/material/Button'
+import { AccessAlarm, ThreeDRotation} from '@mui/icons-material';
+import HomeIcon from '@mui/icons-material/Home';
+import { pink } from '@mui/material/colors';
 function App() {
 
   return (
     <>
-      <h1>Test</h1>
+      <div>duongdeptrai</div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
+      <br />
+      <AccessAlarm/>
+      <ThreeDRotation/>
+      <br />
+      <HomeIcon color="primary" />
+      <HomeIcon color="secondary" />
+      <HomeIcon color="success" />
+      <HomeIcon color="action" />
+      <HomeIcon color="disabled" />
+      <HomeIcon sx={{ color: pink[300] }} />
     </>
   )
 }
