ee9cee4bf06421452dfa31acae4a59bb5106407d
Duong29 <duongnguyenkt99@gmail.com>
2025-07-31 15:04:34 +0700

    Completed

--- src/apis/mock-data.js
+++ src/apis/mock-data.js
@@ -20,7 +20,7 @@ export const mockData = {
             columnId: 'column-id-01',
             title: 'Title of card 01',
             description: 'Markdown Syntax (sẽ ở khóa nâng cao nhé)',
-            cover: 'https://trungquandev.com/wp-content/uploads/2022/07/fair-mern-stack-advanced-banner-trungquandev.jpg',
+            cover: 'https://i.pinimg.com/1200x/5f/cc/80/5fcc80e2a77a4ce69467ea7f08e49fea.jpg',
             memberIds: ['test-user-id-01'],
             comments: ['test comment 01', 'test comment 02'],
             attachments: ['test attachment 01', 'test attachment 02', 'test attachment 03']
